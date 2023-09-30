22987c2c8841dfb11616441c0cedc5a7cb4a120b
barisvarlik <barisvarlik19@gmail.com>
2023-09-30 12:05:56 +0300

    BaseService.cs, BaseMapper.cs and BaseDto classes are implemented

---
+++ PersonalLibrary.API/Mappings/BaseMapper.cs
@@ -0,0 +1,15 @@
+﻿using PersonalLibrary.API.DTOs.Base;
+using PersonalLibrary.Repository.Entities;
+
+namespace PersonalLibrary.API.Mappings;
+
+public abstract class BaseMapper<TEntity, TCreateDto, TReadDto, TUpdateDto>
+    where TEntity : BaseEntity
+    where TCreateDto : IBaseCreateDto
+    where TReadDto : IBaseReadDto
+    where TUpdateDto : IBaseUpdateDto
+{
+    public abstract TEntity ToEntity(TCreateDto dto);
+    public abstract TEntity ToEntity(TUpdateDto dto, TEntity entity);
+    public abstract TReadDto ToDto(TEntity entity);
+}
---
+++ PersonalLibrary.API/Services/BaseService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalLibrary.API.DTOs.Base;
+using PersonalLibrary.API.Mappings;
+using PersonalLibrary.Repository;
+using PersonalLibrary.Repository.Entities;
+
+namespace PersonalLibrary.API.Services;
+
+public abstract class BaseService<TEntity, TCreateDto, TReadDto, TUpdateDto>
+    where TEntity : BaseEntity
+    where TCreateDto : IBaseCreateDto
+    where TReadDto : IBaseReadDto
+    where TUpdateDto : IBaseUpdateDto
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+    protected readonly BaseMapper<TEntity, TCreateDto, TReadDto, TUpdateDto> _mapper;
+
+    protected BaseService(AppDbContext context, BaseMapper<TEntity, TCreateDto, TReadDto, TUpdateDto> mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public abstract Task<ICollection<TEntity>> GetAllAsync();
+    
+    public abstract Task<TEntity> GetByIdAsync(Guid id);
+    
+    public virtual async Task<TReadDto> CreateAsync(TCreateDto dto)
+    {
+        var entity = _mapper.ToEntity(dto);
+        await _context.Set<TEntity>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return _mapper.ToDto(entity);
+    }
+    
+    public virtual async Task UpdateAsync(Guid id, TUpdateDto dto)
+    {
+        var entity = await _context.Set<TEntity>().FirstOrDefaultAsync(x => x.Id == id);
+        
+        if (entity == null) throw new KeyNotFoundException($"Entity not found with id {id}");
+        
+        entity = _mapper.ToEntity(dto, entity);
+        
+        _context.Set<TEntity>().Update(entity);
+        await _context.SaveChangesAsync();
+        
+    }
+    
+    public virtual async Task DeleteAsync(Guid id)
+    {
+        var entity = await _context.Set<TEntity>().FirstOrDefaultAsync(x => x.Id == id);
+        if (entity == null) throw new KeyNotFoundException($"Entity not found with id {id}");
+        
+        _context.Set<TEntity>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+    
+}
